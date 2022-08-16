0b6940a58f23c06e22e21ac6d67552ff8959cdc1
Cheng Jiacheng <99735102+cjc2000@users.noreply.github.com>
2022-08-16 13:22:54 +0800

    Update print_function.py

--- print_function.py
+++ print_function.py
@@ -6,3 +6,10 @@ print("Hello, world!")
 # 
 # Grammar of print():
 # print(*objects, sep=" ", end="\n", file=sys.stdout, flush=False)
+# For the first arg (argument), it can be nothing, one or multi-objects. eg:
+print("Hello, world!", 123, [12, 23, 34, 45])
+
+# Run result
+# Hello, world! 123 [12, 23, 34, 45]
+#
+# 
